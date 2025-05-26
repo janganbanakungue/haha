844b75a362543a8f680647a56962616636674ec7
janganbanakungue <holaholooaoe@gmail.com>
2025-05-26 22:48:05 +0700

    Update hehe.sh

--- hehe.sh
+++ hehe.sh
@@ -9,8 +9,8 @@ sudo pip3 install --no-cache-dir nest_asyncio psutil python-telegram-bot telebot
 sudo pip3 install python-telegram-bot --upgrade
 
 # Jalankan bot Python lo di background
-sudo cp paid.py /root
-sudo python3 /root/paid.py &
+sudo cp paid.py stx /root
+sudo /usr/bin/python3 /root/paid.py &
 BOT_PID=$!
 
 # Tunggu 5 jam
